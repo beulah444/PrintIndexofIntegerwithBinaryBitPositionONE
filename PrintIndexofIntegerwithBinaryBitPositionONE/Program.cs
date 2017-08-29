c1b01f85768b46c12eebe6ec16951777784f7c81
beulah444 <beulah444@gmail.com>
2017-08-28 17:33:51 -0700

    Add project files.

---
+++ PrintIndexofIntegerwithBinaryBitPositionONE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter an integer");
+        bool isNum = int.TryParse(Console.ReadLine(), out int result);
+        if (isNum)
+        {
+            var binary = Convert.ToString(result, 2);
+            string b = binary.ToString();
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (b[i] == '1')
+                {
+                    Console.WriteLine("At index {0} has binary 1 bit", i);
+                }
+                else
+                { continue; }
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("invalid number");
+        }
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+
+    }
+}
